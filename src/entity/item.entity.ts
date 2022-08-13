61536192b64498cef0c5a17907cda74afed71670
11bluetree <lax.bluetree@hostmail.co.jp>
2022-08-13 15:58:51 +0900

    マイグレーション練習

---
+++ src/entity/item.entity.ts
@@ -0,0 +1,26 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Item {
+    @PrimaryGeneratedColumn('uuid') /* 空にした場合数字の連番となる */
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    price: number;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: string;
+
+
+    @Column()
+    createAt: string;
+
+    @Column()
+    updatedAt: string;
+}
