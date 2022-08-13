2d859e6b85bd2245265d2dc2d703cb4bea57ab9b
11bluetree <lax.bluetree@hostmail.co.jp>
2022-08-13 16:00:54 +0900

    場所移動

---
+++ ormconfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+    autoLoadEntities: true,
+    entites: ['dist/entityes/*.entity.js'], /* どのエンティティ情報を読み込むのかの設定
+     基本的にコンパイル済みのファイルを使う */
+    migrations: ['dist/migrations/*.js'], // どのファイルでマイグレーションを行うか
+    cli: { // cliで作成したファイルの出力先
+        ententitiesDir: 'src/entities',
+        migrationsDir: 'src/migrations',
+    },
+}
